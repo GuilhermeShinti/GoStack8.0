3a7eb9139451aa19a5fc42f6cb1921b339af122c
Guilherme Miyashita <gmiyashita@atakone.com.br>
2019-08-23 17:13:34 -0300

    feature: implements crud

---
+++ desafio01/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const projects = []
+
+app.use(express.json())
+
+app.post('/projects', (req, res) => {
+    const project = req.body
+    if (!project.tasks) {
+        project.tasks = []
+    }
+    projects.push(project)
+    return res.json(projects)
+})
+
+app.get('/projects', (req, res) => {
+    return res.json(projects)
+})
+
+app.put('/projects/:id', (req, res) => {
+    const { id } = req.params
+    const { title } = req.body
+    const index = projects.findIndex(p => p.id == id)
+    const project = projects[index]
+    project.title = title
+    res.json(project)
+})
+
+app.delete('/projects/:id', (req, res) => {
+    const { id } = req.params
+    const index = projects.findIndex(p => p.id == id)
+    projects.slice(index, 1)
+    return res.json(projects)
+})
+
+app.post('/projects/:id/tasks', (req, res) => {
+    const { id } = req.params
+    const { title } = req.body
+    const index = projects.findIndex(p => p.id == id)
+    const project = projects[index]
+    project.tasks.push(title)
+    return res.json(project)
+})
+
+app.listen(3000)
